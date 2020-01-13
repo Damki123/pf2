f165b06f5b4df39cccdd9e6ebf7f4b1604d1df48
Damian Chmiel <49138939+Damki123@users.noreply.github.com>
2020-01-13 20:18:07 +0100

    Add files via upload

--- js/nav.js
+++ js/nav.js
@@ -1,11 +1,12 @@
 const navTl = new TimelineMax({paused:true});
 const burger = document.querySelector('#burger');
 const closeNav = document.querySelector('.close');
+const links = document.querySelectorAll('nav li a');
 
 navTl
         .fromTo('nav',.9,{width:0, autoAlpha:0},{width:'100%',autoAlpha:1,ease:Power1.easeInOut})
         .fromTo('nav .container',.6,{width:0, autoAlpha:0},{width:'100%',autoAlpha:1,ease:Power1.easeInOut},'-=.2')
-        .staggerFrom('nav li',.8,{scale:1.25,rotation:180,x:150,opacity:0,ease:Power3.easeInOut},.1)
+        .staggerFrom('nav li',.6,{scale:1.25,autoAlpha:0,ease:Power3.easeInOut},.1)
         .from('.close',.2,{y:25,autoAlpha:0})
 
 
@@ -13,6 +14,28 @@ burger.addEventListener('click',()=>{
     navTl.play().timeScale(1);
 })
 closeNav.addEventListener('click',()=>{
-    navTl.reverse().timeScale(2);
+    navTl.reverse().timeScale(1.5);
 })
+links.forEach(e=>{
+    e.addEventListener('click',()=>{
+        navTl.reverse().timeScale(1);
+    })
+})
+
+
+const logoTl = new TimelineMax();
+const controller = new ScrollMagic.Controller();
+
+logoTl
+    .to('.nameL',.3,{width:0,autoAlpha:0,ease:Power2.easeInOut})
+    .to('#logo img',.3,{height:'40px'})
+
+
+    const scene = new ScrollMagic.Scene({
+    offset: 600,
+    triggerElement: '#s1',
+    reverse: true
+})
+.setTween(logoTl)
+.addTo(controller)
 
