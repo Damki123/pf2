81eff1b70bac70217cbb381cec209854fc1b3335
Damian Chmiel <49138939+Damki123@users.noreply.github.com>
2020-01-17 14:14:49 +0100

    Add files via upload

--- js/anim.js
+++ js/anim.js
@@ -1,5 +1,7 @@
 const s2Photo = new TimelineMax();
 const s2Desc = new TimelineMax();
+const s3Photo = new TimelineMax();
+const s3Desc = new TimelineMax();
 
 s2Photo
     .from('#s2 .photo',2,{x:-200,y:50,autoAlpha:0,ease:Power2.easeOut})
@@ -26,3 +28,27 @@ s2Desc
     .setTween(s2Desc)
     .addTo(controller)
 
+s3Photo
+    .from('#s3 .photo',2,{x:500,y:50,autoAlpha:0,ease:Power2.easeOut})
+
+
+    const sceneS3Photo = new ScrollMagic.Scene({
+        offset: 100,
+        triggerElement: '#s3 .photo',
+        reverse: false,
+    })
+    .setTween(s3Photo)
+    .addTo(controller)
+
+s3Desc
+    .staggerFrom('#s3 .circle',3,{width:0,height:0,ease:Power2.easeOut},.2)
+    .from('#s3 .desc',.3,{autoAlpha:0,ease:Power2.easeOut},'-=2.5')
+
+
+    const sceneS3Desc = new ScrollMagic.Scene({
+        offset: 200,
+        triggerElement: '#s3 .text',
+        reverse: false,
+    })
+    .setTween(s3Desc)
+    .addTo(controller)
